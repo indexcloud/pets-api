519f23c21e2d462e010208246f7898013daca480
Jason Deng <foralief@gmail.com>
2020-11-17 21:52:36 -0600

    First commit

--- server.js
+++ server.js
@@ -1,44 +1,52 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
 
 var app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
 var owners = [
-    {
-        id: 1,
-        name: "Adam",
-        pets: [
-            {
-                id: 1,
-                name: "Vera",
-                type: "Dog"
-            },
-            {
-                id: 2,
-                name: "Felix",
-                type: "Cat"
-            }
-        ]
-    },
-    {
-        id: 2,
-        name: "Kamilah",
-        pets: [
-            {
-                id: 1,
-                name: "Doug",
-                type: "Dog"
-            }
-        ]
-    }
+	{
+		id: 1,
+		name: "Adam",
+		pets: [
+			{
+				id: 1,
+				name: "Vera",
+				type: "Dog",
+			},
+			{
+				id: 2,
+				name: "Felix",
+				type: "Cat",
+			},
+		],
+	},
+	{
+		id: 2,
+		name: "Kamilah",
+		pets: [
+			{
+				id: 1,
+				name: "Doug",
+				type: "Dog",
+			},
+		],
+	},
 ];
 
-
 // GET /api/owners
+app.get("/api/owners", (req, res) => {
+	res.send(owners);
+});
 
 // GET /api/owners/:id
+app.get("/api/owners/:id", (req, res) => {
+	const id = req.params.id;
+	const owner = owners.find(owner => owner.id === Number(id));
+	console.log(owner);
+	res.send(owner);
+});
 
 // POST /api/owners
 
@@ -56,7 +64,6 @@ var owners = [
 
 // DELETE /api/owners/:id/pets/:petId
 
-
-app.listen(3000, function(){
-    console.log('Pets API is now listening on port 3000...');
-})
+app.listen(3001, function () {
+	console.log("Pets API is now listening on port 3001...");
+});
